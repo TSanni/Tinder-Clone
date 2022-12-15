05291b0554d4ba959f55c00d00d00d8f77b6d089
TSanni <tomas.sanni@gmail.com>
2022-12-15 16:40:50 -0600

    Initial Commit

---
+++ Tinder Clone/Tinder_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tinder_CloneApp.swift
+//  Tinder Clone
+//
+//  Created by Tomas Sanni on 12/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Tinder_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
